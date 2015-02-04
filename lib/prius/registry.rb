a0a1e61e4b40a72fc3756c68c2f9076585de62a1
Harry Marr <harry.marr@gmail.com>
2015-02-04 15:04:04 +0000

    Allow nillable values

--- lib/prius/registry.rb
+++ lib/prius/registry.rb
@@ -9,12 +9,12 @@ module Prius
       @registry = {}
     end
 
-    def load(name, env_var: nil, type: :string)
+    def load(name, env_var: nil, type: :string, allow_nil: false)
       env_var ||= name.to_s.upcase
       @registry[name] = case type
-                        when :string then load_string(env_var)
-                        when :int    then load_int(env_var)
-                        when :bool   then load_bool(env_var)
+                        when :string then load_string(env_var, allow_nil)
+                        when :int    then load_int(env_var, allow_nil)
+                        when :bool   then load_bool(env_var, allow_nil)
                         else raise ArgumentError
                         end
     end
@@ -33,22 +33,27 @@ module Prius
       end
     end
 
-    def load_string(name)
+    def load_string(name, allow_nil)
       @env.fetch(name)
     rescue KeyError
+      return nil if allow_nil
       raise MissingValueError, "config value '#{name}' not present"
     end
 
-    def load_int(name)
-      value = load_string(name)
+    def load_int(name, allow_nil)
+      value = load_string(name, allow_nil)
+      return value if value.nil?
+
       unless /\A[0-9]+\z/.match(value)
         raise TypeMismatchError, "'#{name}' value '#{value}' is not an integer"
       end
       value.to_i
     end
 
-    def load_bool(name)
-      value = load_string(name)
+    def load_bool(name, allow_nil)
+      value = load_string(name, allow_nil)
+      return nil if value.nil?
+
       if /\A(yes|y|true|t|1)\z/i.match(value)
         return true
       elsif /\A(no|n|false|f|0)\z/i.match(value)
