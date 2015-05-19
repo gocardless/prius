88d3ec281a064d438e4122fcfe578f669a5bba0a
Grey Baker <greysteil@gmail.com>
2015-05-19 20:32:57 +0100

    Clean up docs

--- lib/prius.rb
+++ lib/prius.rb
@@ -2,15 +2,15 @@ require "prius/registry"
 require "prius/railtie" if defined?(Rails)
 
 module Prius
-  # Load an environment variable in to the registry.
+  # Load an environment variable into the registry.
   #
   # name    - The Symbol name of the item in the registry. Use this when
-  #           looking the item up via `#get`. By if `env_var` is not provided,
+  #           looking up the item via `#get`. If `env_var` is not provided
   #           this will be uppercased and used as the environment variable name.
   # options - An optional Hash of options (default {}):
   #           :env_var  - The String name of the environment variable. If
-  #                       omitted, the uppercased form of `name` will be used.
-  #           :type     - The Symbol type of the environment variable's value
+  #                       omitted the uppercased form of `name` will be used.
+  #           :type     - The Symbol type of the environment variable's value.
   #                       The value will be coerced to this type. Must be one
   #                       of :string, :int, or :bool (default :string).
   #           :required - A Boolean indicating whether the value must be
@@ -21,8 +21,8 @@ module Prius
   #                       variable isn't present (default true).
   #
   # Raises a MissingValueError for required values that are missing.
-  # Raises a TypeMismatchError if a value can't be coerced to the correct type.
-  # Raises an ArgumentError if an invalid type is provided.
+  # Raises a TypeMismatchError if a value can't be coerced to the given `type`.
+  # Raises an ArgumentError if an invalid `type` is provided.
   def self.load(name, options = {})
     registry.load(name, options)
   end
